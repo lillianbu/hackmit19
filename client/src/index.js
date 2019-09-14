ccf5db72e52a54cc583fd8195a388e22ef419622
Lillian Bu <lbu5@mit.edu>
2019-09-14 17:03:09 -0400

    add css file

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.js';
 
+const Root = () => (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
 ReactDOM.render(
-    <App />,
-    document.getElementById("root")
+    <Root />,
+    document.getElementById('root')
 );
 
 module.hot.accept();
