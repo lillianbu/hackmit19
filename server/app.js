910ead2578bb9ee9c37157568be43fd7e1e4a4aa
Sayan Chatterjee <sayanc@berkeley.edu>
2019-09-15 01:30:58 -0400

    fixed minor bugs

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.static(publicPath));
 app.use(express.urlencoded());
 app.use(express.json());
 
-http.listen(3000, () => {
+http.listen(5000, () => {
 	console.log(`Listening on port 3000 and looking in folder ${publicPath}`);
 });
 
@@ -27,16 +27,16 @@ const { RevAiApiClient } = require('revai-node-sdk');
 var accessToken = "02iwmNeyKMeo1GUBcrjQUJxBTHJA14Z83bq1ISFR_i_slryM-xwKOTvKUmLxl7octj-bMdQ4ye9d3BvFNzqE5ER-wVC4s";
 var client = new RevAiApiClient(accessToken);
 var in_progress = 0;
-var path = __dirname;
+var musicPath = __dirname;
 // navigator.mediaDevices.getUserMedia({audio:true}).then(stream => {handlerFunction(stream)})
 // var job = await client.submitJobLocalFile("./audio/test_file.mp3");
 // var transcriptText = await client.getTranscriptText(job.id);
 // console.log(transcriptText)
 app.get('/processText', async (request, response) => {
 	// "/Users/Sayan/Desktop/hackmit19/hackmit19/server/audio/test_file.mp3"
-	console.log(path);
+	console.log(musicPath);
 	if (in_progress == 0) {
-		var job = await client.submitJobLocalFile(path);
+		// var job = await client.submitJobLocalFile(path);
 		in_progress = 1;
 		response.send('Processing audio...');
 	}
@@ -59,7 +59,7 @@ app.get('/getText', async (request, response) => {
 app.post('/record', (request, response) => {
 	// response.send("record triggered");
 	const title = request.body.title;
-	path = __dirname.concat("/audio/", title, ".mp3");
+	musicPath = __dirname.concat("/audio/", title, ".mp3");
 	console.log(title);
 	// rec.start();
 })
